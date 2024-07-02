6a3aa1dcadd30753ba2fe19ecd2c1f2c2b0b812a
Raphael Silva <Raphael010601@gmail.com>
2024-07-02 20:43:54 -0300

    up

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ const itens = document.querySelectorAll('.carrousel-item');
 const totalItens = itens.length;
 
 // Define quantos itens devem ser visíveis ao mesmo tempo no carrossel
-let visibleItens = 1;
+let visibleItens = 3;
 
 // Calcula o índice máximo que pode ser alcançado sem ultrapassar os limites do carrossel
 let maxIndex = totalItens - visibleItens;
@@ -49,7 +49,7 @@ function prevSlide() {
 // Define a função que atualiza a posição do carrossel na tela
 function updateCarousel() {
     // Calcula o deslocamento necessário para mostrar o item atual na tela
-    let offset = currentIndex * -310; // 300px de largura do item + 35px de margem entre os itens
+    let offset = currentIndex * -335; // 300px de largura do item + 35px de margem entre os itens
     // Atualiza o estilo CSS do elemento com a classe 'carousel-inner' para mover o carrossel
    
     document.querySelector('.carrousel-inner').style.transform = `translateX(${offset}px)`;
